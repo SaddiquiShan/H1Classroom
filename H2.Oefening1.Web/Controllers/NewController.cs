d52f63d23b1b4cd409210ac88916f6a05eefb5b6
Shan <shan.saddiqui@student.howest.be>
2019-04-22 18:48:59 +0200

    Add NewController: Index: Return Content 3 sites of series text/html Whatsmyname: Return Shan Saddiqui, text/plain

---
+++ H2.Oefening1.Web/Controllers/NewController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace H2.Oefening1.Web.Controllers
+{
+    public class NewController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            return Content
+                ("<ul><li><a href='https://www.rottentomatoes.com/'>Rotten Tomatoes</a></li>" +
+                "<li><a href='https://worldofwarcraft.com/en-us/'>World of Warcraft</a></li>" +
+                "<li><a href='https://darksiders.com/'>Darksiders</a></li>" +
+                "<li><a href='new/whatsmyname'>Whats my name?</a></li></ul>", "text/html");
+        }
+
+        public IActionResult Whatsmyname()
+        {
+            return Content("Shan Saddiqui", "text/plain");
+        }
+
+    }
+}
